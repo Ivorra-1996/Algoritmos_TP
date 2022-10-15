c2f3dea1779b5cdc60f0d3baf00cd7917a3bcd7e
Ivorra-1996 <ivorrajosemaria@gmail.com>
2022-10-15 15:54:04 -0300

    modificando funcion escribir archivo

--- main.cpp
+++ main.cpp
@@ -18,11 +18,32 @@ void leerArchivo(){
     else{
         cout<<"Error en leer el archivo...";
     }
-    
+}
 
+void escribirArchivo(){
+    archivo.open("terminales.txt",ios::out);
+    if(archivo.is_open()){
+        cout<<"Escriba exit para terminar el archivo..\n";
+        string salida_linea_texto;
+        while(true){
+            string linea; //lee cada linea
+            getline(cin,linea);
+            if(linea == "exit"){
+                break;
+            }
+            salida_linea_texto += linea+'\n';    
+        }
+        archivo<<salida_linea_texto;
+        archivo.close();
+    }
+    else{
+        cout<<"No se encontro el archivo";
+    }
 }
 
 int main(){
+    leerArchivo();
+    escribirArchivo();
     leerArchivo();
     system("pause");
     return 0 ;
