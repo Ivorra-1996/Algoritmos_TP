aea4954bfcdd72d765622acde3de0aaafacebe87
Ivorra-1996 <ivorrajosemaria@gmail.com>
2022-10-15 14:58:06 -0300

    Agregando funcion leer archivo y main

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+fstream archivo;
+
+void leerArchivo(){
+    archivo.open();
+    string linea;
+
+}
+
+int main(){
+    system("pause");
+    return 0 ;
+}
