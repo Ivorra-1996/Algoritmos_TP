95e3510b7d08a0cbdb9a42ad4fcdc32233cac3c2
Ivorra-1996 <ivorrajosemaria@gmail.com>
2022-10-30 19:33:13 -0300

    Se agrego el archivo menu y terminales

---
+++ menu.cpp
@@ -0,0 +1,47 @@
+#include "terminales.cpp"
+
+
+
+
+void menu(){
+    int opcion=0;
+    do{
+        cout<<"Manejo de Archivos en C++"<<endl;
+        cout<<"1. Altas"<<endl;
+        cout<<"2. Bajas"<<endl;
+        cout<<"3. Consultas"<<endl;
+        cout<<"4. Buscar un Registro"<<endl;
+        cout<<"5. Salir"<<endl;
+        cout<<"Que deseas hacer? ";
+        cin>>opcion;
+        switch(opcion){
+            case 1:
+                altas();
+                break;
+            case 2:
+                bajas();
+                break;
+            case 3:
+                leerArchivo();
+                break;
+            case 4:
+                buscarTerminal();
+                break;
+            case 5:
+                cout<<"Programa Finalizado"<<endl;
+                break;
+            default:
+                cout<<"¡Opcion Incorrecta!"<<endl;
+        }
+    }while(opcion!=5);
+}//Fin funcion menu
+
+
+int main(){
+    menu();
+    return 0;
+    system("PAUSE");
+}
+
+
+// Problemas con los espacios a la hora de ingresar las terminales por alta.
